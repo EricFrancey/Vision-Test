3c99de5fa5f981baacd55d526148830429069906
Eric Francey <eric.g.francey@gmail.com>
2022-10-27 23:43:37 -0400

    UI update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-[LIVE DEMO](https://ericfrancey.github.io/drag-drop-photo-editor/)
+# Vision Test
+
+## Live Demo
+[Vision Test](https://ericfrancey.github.io/drag-drop-photo-editor/)
+
+UI built with HTML, CSS, JavaScript.\
+Gradient help by [Ben Howdle](https://github.com/benhowdle89/grade).
+
+```md
+Test your vision: Drag a photo to the box to begin, use your mouse to change perspective and the buttons to change colour. Once placed, you can change the size and convert to a gradient background that uses the main colours from the picture.
+```
+
--- script.js
+++ script.js
@@ -1,49 +1,41 @@
 const dropzone = document.getElementById('outer');
 const picture = document.querySelector('img');
 const droptext = document.querySelector('.droptext')
-
 let sizeSlider = document.getElementById("image-size");
 
 sizeSlider.oninput = function() {
   if (sizeSlider.value == 1) {
-    dropzone.children[0].style.height = "20%";
-    dropzone.children[0].style.width = "20%";
+    dropzone.children[0].style.width = "20rem";
   }
   if (sizeSlider.value == 2) {
-    dropzone.children[0].style.height = "40%";
-    dropzone.children[0].style.width = "40%";
+    dropzone.children[0].style.width = "25rem";
   }
   if (sizeSlider.value == 3) {
-    dropzone.children[0].style.height = "60%";
-    dropzone.children[0].style.width = "60%";
+  
+    dropzone.children[0].style.width = "30rem";
   }
   if (sizeSlider.value == 4) {
-    dropzone.children[0].style.height = "80%";
-    dropzone.children[0].style.width = "80%";
+   
+    dropzone.children[0].style.width = "35rem";
   }
   if (sizeSlider.value == 5) {
-    dropzone.children[0].style.height = "100%";
-    dropzone.children[0].style.width = "100%";
+    
+    dropzone.children[0].style.width = "40rem";
   }
   if (sizeSlider.value == 6) {
-    dropzone.children[0].style.height = "110%";
-    dropzone.children[0].style.width = "110%";
+    dropzone.children[0].style.width = "45rem";
   }
   if (sizeSlider.value == 7) {
-    dropzone.children[0].style.height = "120%";
-    dropzone.children[0].style.width = "120%";
+    dropzone.children[0].style.width = "50rem";
   }
   if (sizeSlider.value == 8) {
-    dropzone.children[0].style.height = "130%";
-    dropzone.children[0].style.width = "130%";
+    dropzone.children[0].style.width = "55rem";
   }
   if (sizeSlider.value == 9) {
-    dropzone.children[0].style.height = "140%";
-    dropzone.children[0].style.width = "140%";
+    dropzone.children[0].style.width = "60rem";
   }
   if (sizeSlider.value == 10) {
-    dropzone.children[0].style.height = "150%";
-    dropzone.children[0].style.width = "150%";
+    dropzone.children[0].style.width = "65rem";
   }
 }
 
@@ -69,9 +61,6 @@ dropzone.addEventListener('drop', event => {
   const file = event.dataTransfer.files[0];
   const reader = new FileReader();
   reader.readAsDataURL(file);
-
-  // console.log(file)
-  // console.log(reader)
  
   reader.addEventListener('loadend', () => {
     const img = document.createElement('img');
@@ -79,16 +68,10 @@ dropzone.addEventListener('drop', event => {
     console.log(reader.result);
     dropzone.removeChild(dropzone.firstElementChild);
     dropzone.append(img);
-    img.classList.add('image-sizing');
- 
-  //  console.log(dropzone);
-// dropzone.classList.add("dropped");
-// if (dropzone.classList.contains("dropped")) {
-//   Grade(document.querySelectorAll('.gradient-wrap'))
-// }
-  droptext.classList.add("hidden");
+    dropzone.style.backgroundColor="rgb(58, 58, 58)";
+    droptext.classList.add("hidden");
 
-  const tilt = document.querySelector(".tilt");
+    const tilt = document.querySelector(".tilt");
 
     tilt.addEventListener("mousemove", event => {
       const { top, bottom, left, right } = event.target.getBoundingClientRect();
@@ -137,4 +120,31 @@ function removeAllClasses() {
   dropzone.classList.remove("sepia");
   dropzone.classList.remove("invert");
   dropzone.classList.remove("hue");
+}
+
+function eyeTest() {
+  const img = document.createElement('img');
+  img.src = "Snellen_chart.png";
+  dropzone.removeChild(dropzone.firstElementChild);
+  dropzone.append(img);
+  dropzone.style.backgroundColor="rgb(58, 58, 58)";
+  droptext.classList.add("hidden");
+
+  const tilt = document.querySelector(".tilt");
+
+  tilt.addEventListener("mousemove", event => {
+    const { top, bottom, left, right } = event.target.getBoundingClientRect();
+  
+    const middleX = (right - left) / 2;
+    const middleY = (bottom - top) / 2;
+  
+    const clientX = event.clientX;
+    const clientY = event.clientY;
+  
+    const offsetX = (clientX - middleX) / middleX;
+    const offsetY = (middleY - clientY) / middleY;
+  
+    event.target.style.transform = `perspective(1000px) rotateY(${offsetX *
+      5}deg) rotateX(${offsetY * 5}deg) scale3d(1, 1, 1)`;
+  });
 }
